d68e847c13c86c03951394ca562b7621156e915c
MellerPeller <melker.eriksson@skola.sala.se>
2024-05-21 11:22:21 +0200

    Add project files.

---
+++ Rövarspråk/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Rövarspråk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(47, 168);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(268, 164);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_changed);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(470, 168);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 164);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Svenska";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(466, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rövarspråk";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(321, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "==>";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Rövarspråk/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rövarspråk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        
+
+        private void textBox1_changed(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text;
+            string translatedText = TranslateToRovarsprak(inputText);
+            textBox2.Text = translatedText;
+        }
+
+        //Metod som gör översättningen från svenska till rövarspråk
+        private string TranslateToRovarsprak(string input)
+        {
+            // Fixar så att x/X behandlas som ks/KS (Istället för xox blir det koksos)
+            input = input.Replace("x", "ks").Replace("X", "KS");
+
+            // Matchar alla konsonanter både i små och stora bokstäver
+            string pattern = @"[bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ]";
+            // Tar varje matchad konsonant och lägger till o sedan konsonanten igen
+            string replacementEvaluator(Match m)
+            {
+                string consonant = m.Value;
+                return consonant + "o" + consonant.ToLower();
+            }
+
+            /* Använder texten från textbox1 och mönstret (pattern variabeln) för att sedan
+               byta ut texten med den översatta texten från replacementEvaluator fuctionen 
+               replacementEvaluator använder bara konsonanter.                             */
+            string result = Regex.Replace(input, pattern, new MatchEvaluator(replacementEvaluator));
+            return result;
+        }
+    }
+}
+
